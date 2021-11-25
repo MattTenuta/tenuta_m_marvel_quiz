f72c7d58040e3a0a72b85093fa537138519e7004
Matt Tenuta <m_tenuta@fanshaweonline.ca>
2021-11-25 16:17:15 -0500

    added _init_.py

---
+++ main.py
@@ -0,0 +1,27 @@
+print("==============================")
+print("Welcome to my Marvel character quiz made using python")
+print("Follow these simple rules if you want to survive")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
