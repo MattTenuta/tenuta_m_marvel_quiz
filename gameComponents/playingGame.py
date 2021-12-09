b73d0119bd59509e8bd77373e4ba00aba21f5290
Matt Tenuta <m_tenuta@fanshaweonline.ca>
2021-12-09 13:39:06 -0500

    added more questions

--- gameComponents/playingGame.py
+++ gameComponents/playingGame.py
@@ -4,7 +4,7 @@ def game():
     gameVars.avengerPoints = 0
     print("Here is your first question")
     print("==============================")
-    Q1 = input("What superhero was created as a super soldier to fight in WW2? Was it a) Iron Man, b) Captain America, c) The Hulk, or d) Black Widow ")
+    Q1 = input("What Superhero was created as a super soldier to fight in WW2? Was it a) Iron Man, b) Captain America, c) The Hulk, or d) Black Widow ")
 
     if Q1 == "b":
         print("==============================")
@@ -64,13 +64,61 @@ def game():
         print("Wow, you really got that one wrong?")
         print("No Avenger points for you, Dirtbag")
         print("Avenger Points: " + str(gameVars.avengerPoints))
+
+    print("==============================")
+    print("Next Question")
+    print("==============================")
+    Q5 = input("In the first Spiderman movie, Peters best friend was Harry Osborne. Which Villan was Harry's Father? Was it a) SandMan, b) Dr. Otto Octavious, c) Green Goblin, or d) Venom ")
+        
+    if Q5 == "c":
+        print("==============================")
+        print("Excellent Work!")
+        gameVars.avengerPoints = gameVars.avengerPoints + 1
+        print("Avenger Points: " + str(gameVars.avengerPoints))
+    else:
+        print("==============================")
+        print("You suck")
+        print("No Avenger points for you, Dirtbag")
+        print("Avenger Points: " + str(gameVars.avengerPoints))
+
+    print("==============================")
+    print("Next Question")
+    print("==============================")
+    Q6 = input("What Marvel character does not age and wears an eyepatch? Is it a) Jack Sparrow, b) Nick Fury, c) Bruce Banner, or d) Hawkeye ")
+        
+    if Q6 == "b":
+        print("==============================")
+        print("Why didn't you guess Jack Sparrow? Just kidding, great work!")
+        gameVars.avengerPoints = gameVars.avengerPoints + 1
+        print("Avenger Points: " + str(gameVars.avengerPoints))
+    else:
+        print("==============================")
+        print("Good try, NOT.")
+        print("No Avenger points for you, Dirtbag")
+        print("Avenger Points: " + str(gameVars.avengerPoints))
+
+    print("==============================")
+    print("Next Question")
+    print("==============================")
+    Q7 = input("What Marvel characters real name is James Howlett and first appeared in an issue of the Incredible Hulk Comic Book? Is it a) Wolverine, b) Spiderman, c) Deadpool, or d) Black Panther ")
+        
+    if Q7 == "a":
+        print("==============================")
+        print("Damn, you must have read the comic to know that, or you are just a huge Marvel fan")
+        gameVars.avengerPoints = gameVars.avengerPoints + 1
+        print("Avenger Points: " + str(gameVars.avengerPoints))
+    else:
+        print("==============================")
+        print("Wow, you really got that one wrong?")
+        print("No Avenger points for you, Dirtbag")
+        print("Avenger Points: " + str(gameVars.avengerPoints))
         
     print("==============================")
     print("Next Question")
     print("==============================")
-    Q5 = input("What Marvel character is a master strategist and problem solver who is an expert in close-quarter combat, various human and alien firearms, and battle techniques? Is it a) Groot, b) Star Lord, c) Doctor Strange, or d) Captain Marvel ")
+    Q8 = input("What Marvel character is a master strategist and problem solver who is an expert in close-quarter combat, various human and alien firearms, and battle techniques? Is it a) Groot, b) Star Lord, c) Doctor Strange, or d) Captain Marvel ")
         
-    if Q5 == "b":
+    if Q8 == "b":
         print("==============================")
         print("Great Work, You are amazing!")
         gameVars.avengerPoints = gameVars.avengerPoints + 1
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from gameComponents import playingGame
 
 print("==============================")
-print("Welcome to my Marvel character quiz made using Python")
+print("Welcome to my Marvel Character Quiz made using Python")
 print("==============================")
 print("Follow these simple rules if you want to survive.")
-print("You will be asked a question about a Marvel character and you must provide the correct answer.")
+print("You will be asked various questions about Marvel characters and you must provide the correct answers.")
 print("For each correct answer you will earn 1 Avenger Point.")
 print("At the end, the computer will determine if you are a real Avenger or not!")
 print("And one other thing, when typing your answers, use lower case letters or it will not work.")
